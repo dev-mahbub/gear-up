e5ca632a119e9c13aa8ae9efba59a6c1bb6c53a3
dev-mahbub <mahbub.devs@gmail.com>
2026-07-07 22:38:41 +0600

    category route protection added

--- src/modules/category/category.route.ts
+++ src/modules/category/category.route.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { categoryController } from "./category.controller";
+import { auth } from "../../middleware/auth";
+import { Role } from "../../../prisma/generated/prisma/enums";
 
 const router = Router();
 
-router.post("/", categoryController.createCategory);
+router.post("/", auth(Role.PROVIDER), categoryController.createCategory);
 
 export const categoryRoutes = router;
